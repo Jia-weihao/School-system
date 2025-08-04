d2ae1043e05d43116b62c3bd6d22ca9d95d5670a
贾维浩 <15895372+javi-jiaweihao@user.noreply.gitee.com>
2025-08-04 18:49:35 +0800

    修改代码

--- app/tools/api.ts
+++
@@ -1,7 +0,0 @@
-// API基础URL配置
-// 根据当前环境设置API基础URL
-const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://school.blxg.asia'
-  : 'http://localhost:3000';
-
-export default API_BASE_URL;
--- app/tools/api.tsx
+++ app/tools/api.tsx
@@ -1,2 +1,12 @@
-const api = "http://localhost:3000";
-export default api;
+// API基础URL配置
+// 根据当前环境设置API基础URL
+declare const process: {
+  env: {
+    NODE_ENV: string;
+  };
+};
+const API_BASE_URL = process.env.NODE_ENV === 'production'
+  ? 'https://school.blxg.asia'
+  : 'http://localhost:3000';
+
+export default API_BASE_URL;
