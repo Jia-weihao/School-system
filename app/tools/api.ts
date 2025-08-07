3852ff6434a9ff076ad8376f6399342cfe322ad6
贾维浩 <15895372+javi-jiaweihao@user.noreply.gitee.com>
2025-08-08 00:25:48 +0800

    qingkong

--- app/tools/api.ts
+++
@@ -1,6 +0,0 @@
-// API基础URL配置
-// 根据当前环境设置API基础URL
-const API_BASE_URL= 'https://school.blxg.asia'
- 
-
-export default API_BASE_URL;
--- app/tools/api.tsx
+++
@@ -1,3 +0,0 @@
-const api = "https://school.blxg.asia";
-
-export default api;
